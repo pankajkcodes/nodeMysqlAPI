f2fb3435a9bd94bcc69db84f7192ec7cd962ff55
Pankajkcodes <74593517+pankajkcodes@users.noreply.github.com>
2022-10-23 12:38:07 +0530

    Add files via upload
    Technology used :
    - Node.js, Express.js
    - Database :  Mysql

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+    
+      host: "db4free.net",
+      user: "nodemysql1",
+      password: "nodemysql1",
+      database: "nodemysql1",
+    },
+    listPerPage: 20,
+  };
+  module.exports = config;
